6d934bf912b73296d1d2c711e39a8a8153d0cba4
Arjan Einbu <arjan@einbu.no>
2017-11-07 14:43:50 +0100

    Remove unused require

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const commonConfig = require("./webpack.common.config");
-const nodeExternals = require("webpack-node-externals");
 const merge = (...objs) => require("deepmerge").all(objs, {arrayMerge: (arr1, arr2) => arr1.concat(arr2) });
 
 const combinedConfig = merge({}, commonConfig, {
