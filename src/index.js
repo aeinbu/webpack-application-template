0d4e64b303a376ed4172a0bb3c441873cf2118d0
robin <2796968+ctrl-alt-null@users.noreply.github.com>
2019-12-23 03:25:00 +0100

    Added support for loading SCSS (#3)
    * Added support for `.scss`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-require("@babel/polyfill")
+require("@babel/polyfill");
+require("src/styles/scss/master.scss");
 
 import _ from "lodash";
 
 console.log("...and this JS message should be seen in the browser console!");
-
